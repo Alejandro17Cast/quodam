16a6589c8f41a88d6ddf64148ecf52b0a0fa978b
Alejandro Castañeda <luisalejandrocasta3@gmail.com>
2026-08-09 17:56:19 -0600

    chore: add application configuration

---
+++ js/config.js
@@ -0,0 +1,13 @@
+export const CONFIG = {
+  storiesIndexPath: "./data/stories-index.json",
+
+  ritualDuration: 1800,
+
+  selection: {
+    totalRounds: 18, //cambiar si se quiere que dure más o menos la sesión
+    initialDelay: 90,
+    delayIncrement: 22
+  },
+
+  avoidImmediateRepeat: true
+};
