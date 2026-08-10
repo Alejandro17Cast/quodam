f8cb123e7529d363e1e0a66a7c4f9f1f0c561a32
Alejandro Castañeda <luisalejandrocasta3@gmail.com>
2026-08-09 20:13:48 -0600

    refactor: centralize reading image path configuration

--- js/config.js
+++ js/config.js
@@ -13,3 +13,23 @@ export const CONFIG = {
 
   avoidImmediateRepeat: true
 };
+
+export const CONFIG = {
+  readingsIndexPath:
+    "./data/readings-index.json",
+
+  imagesPath:
+    "./assets/images/readings",
+
+  ritualDuration: 1600,
+
+  selection: {
+    totalRounds: 18,
+    initialDelay: 70,
+    minimumDelay: 70,
+    maximumDelay: 520,
+    slowdownStart: 9
+  },
+
+  avoidImmediateRepeat: true
+};
